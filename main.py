2ebd10a47c54c846672ad10f91f79e958abffce6
Argen <65073960+ArgenTeshebaev@users.noreply.github.com>
2020-12-16 07:54:09 +0600

    Final version

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-#width & height
+# width & height
 screen_width = 600
 screen_height = 600
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Game')
+
+font = pygame.font.SysFont('Arial', 30)
 
 bg = (69, 69, 81)
 block_light = (230, 190, 231)
@@ -17,10 +19,21 @@ block_bottom = (146, 32, 149)
 board_col = (250, 250, 250)
 board_outline = (200, 200, 200)
 
+text = (255, 255, 255)
+
 cols = 6
 rows = 6
 clock = pygame.time.Clock()
 fps = 60
+
+live_ball = False
+game_over = 0
+
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
 
 class wall():
 
@@ -62,16 +75,11 @@ class wall():
                 pygame.draw.rect(screen, block_col, block[0])
                 pygame.draw.rect(screen, bg, (block[0]), 2)
 
+
 class board():
 
     def __init__(self):
-        self.height = 20
-        self.width = int(screen_width / cols)
-        self.x = int((screen_width / 2) - (self.width / 2))
-        self.y = screen_height - (self.height * 2)
-        self.speed = 10
-        self.rect = Rect(self.x, self.y, self.width, self.height)
-        self.direction = 0
+        self.reset()
 
     def moving(self):
         self.direction = 0
@@ -83,23 +91,23 @@ class board():
             self.rect.x += self.speed
             self.direction = 1
 
-
-
-
     def draw(self):
         pygame.draw.rect(screen, board_col, self.rect)
         pygame.draw.rect(screen, board_outline, self.rect, 3)
 
+    def reset(self):
+        self.height = 20
+        self.width = int(screen_width / cols)
+        self.x = int((screen_width / 2) - (self.width / 2))
+        self.y = screen_height - (self.height * 2)
+        self.speed = 10
+        self.rect = Rect(self.x, self.y, self.width, self.height)
+        self.direction = 0
+
+
 class game_ball():
     def __init__(self, x, y):
-        self.ball_rad = 10
-        self.x = x - self.ball_rad
-        self.y = y
-        self.rect = Rect(self.x,self.y, self.ball_rad * 2, self.ball_rad * 2)
-        self.speed_x = 4
-        self.speed_y = -4
-        self.speed_max = 5
-        self.game_over = 0
+        self.reset(x, y)
 
     def moving(self):
         collision_thresh = 5
@@ -113,11 +121,11 @@ class game_ball():
                     if abs(self.rect.bottom - item[0].top) < collision_thresh and self.speed_y > 0:
                         self.speed_y *= -1
                     if abs(self.rect.top - item[0].bottom) < collision_thresh and self.speed_y < 0:
-                         self.speed_y *= -1
+                        self.speed_y *= -1
                     if abs(self.rect.right - item[0].left) < collision_thresh and self.speed_y > 0:
                         self.speed_x *= -1
                     if abs(self.rect.left - item[0].right) < collision_thresh and self.speed_x < 0:
-                         self.speed_x *= -1
+                        self.speed_x *= -1
                     if wall.blocks[row_count][item_count][1] > 1:
                         wall.blocks[row_count][item_count][1] -= 1
                     else:
@@ -141,7 +149,6 @@ class game_ball():
         if self.rect.bottom > screen_height:
             self.game_over = -1
 
-
         if self.rect.colliderect(player_board):
             if abs(self.rect.bottom - player_board.rect.top) < collision_thresh and self.speed_y > 0:
                 self.speed_y *= -1
@@ -160,7 +167,18 @@ class game_ball():
 
     def draw(self):
         pygame.draw.circle(screen, board_col, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad), self.ball_rad)
-        pygame.draw.circle(screen, board_outline, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad), self.ball_rad, 3)
+        pygame.draw.circle(screen, board_outline, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad),
+                           self.ball_rad, 3)
+
+    def reset(self, x, y):
+        self.ball_rad = 10
+        self.x = x - self.ball_rad
+        self.y = y
+        self.rect = Rect(self.x, self.y, self.ball_rad * 2, self.ball_rad * 2)
+        self.speed_x = 4
+        self.speed_y = -4
+        self.speed_max = 5
+        self.game_over = 0
 
 
 wall = wall()
@@ -181,18 +199,34 @@ while run:
     wall.draw_wall()
 
     player_board.draw()
-    player_board.moving()
 
     ball.draw()
-    ball.moving()
+
+    if live_ball:
+        player_board.moving()
+
+        game_over = ball.moving()
+        if game_over != 0:
+            live_ball = False
+
+    if not live_ball:
+        if game_over == 0:
+            draw_text('CLICK ANYWHERE TO START', font, text, 100, screen_height // 2 + 100)
+        elif game_over == 1:
+            draw_text('YOU WON!', font, text, 240, screen_height // 2 + 50)
+            draw_text('CLICK ANYWHERE TO START', font, text, 100, screen_height // 2 + 100)
+        elif game_over == -1:
+            draw_text('YOU LOST!', font, text, 240, screen_height // 2 + 50)
+            draw_text('CLICK ANYWHERE TO START', font, text, 100, screen_height // 2 + 100)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-    pygame.display.update()
-
-    pygame.display.update()
+        if event.type == pygame.MOUSEBUTTONDOWN and live_ball == False:
+            live_ball = True
+            ball.reset(player_board.x + (player_board.width // 2), player_board.y - player_board.height)
+            player_board.reset()
+            wall.create_wall()
 
     pygame.display.update()
 
